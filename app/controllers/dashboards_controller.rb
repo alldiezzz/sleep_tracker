8b2f9619ab59d5817bdf2a2f8252596cd97524b3
alldiezzz <aldi.reinaldi.muhamad@gmail.com>
2025-01-24 13:26:30 +0700

    add db seed

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,8 @@
 class DashboardsController < ApplicationController
   before_action :set_user, only: :index
   def index
-    @sleep_sessions = @user.sleep_sessions
-    @followed_sleep_sessions = @user.followed.map(&:sleep_sessions).flatten
+    User.reindex_sleep_sessions
+    @sleep_sessions = IndexService.get("sleep_session_user_#{@user.id}")&.dig(:sleep_sessions) || []
+    @followed_sleep_sessions = @user.followed.flat_map { |fu| IndexService.get("sleep_session_user_#{fu.id}") }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Create a user
+user_names = %w[
+  John Doe Jane Smith Alice Brown Bob White Charlie Black David Green
+  Emily Johnson Michael Williams Sarah Jones Chris Miller Jessica Davis
+  Daniel Wilson Laura Moore Kevin Taylor Megan Anderson Brian Thomas
+  Amanda Jackson
+]
+
+user_names.each do |name|
+  User.find_or_create_by!(name: name)
+end
+
+User.all.each do |user|
+  start_time = Time.current - rand(1..30).days - rand(1..24).hours - rand(1..60).minutes
+  end_time = start_time + rand(6..8).hours
+  sleep_session = user.sleep_sessions.create!(start_time: start_time, end_time: end_time, total_duration_in_seconds: end_time - start_time)
+
+  if rand(0..1) == 1
+    sleep_session.sleep_events.create!(event_type: "wake_up", event_time: start_time + 2.hour)
+    sleep_session.sleep_events.create!(event_type: "fall_asleep", event_time: start_time + 150.minutes)
+    sleep_session.sleep_events.create!(event_type: "wake_up", event_time: start_time + end_time + 1.hour)
+  else
+    sleep_session.sleep_events.create!(event_type: "wake_up", event_time: start_time + end_time + 1.hour)
+  end
+end
