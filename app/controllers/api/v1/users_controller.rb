95654d182d6e4435a2ffcb2a20944816b54c190a
alldiezzz <aldi.reinaldi.muhamad@gmail.com>
2025-01-24 07:15:46 +0700

    added follow and unfollow function

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,37 @@
 class Api::V1::UsersController < Api::V1::BaseController
+  def follow
+    follow = Follow.find_or_initialize_by(
+      follower_id: follow_params[:follower_id],
+      followed_id: follow_params[:followed_id]
+    )
+
+    if follow.persisted?
+      render json: { message: "You are already following this user" }, status: :ok
+    elsif follow.save
+      render json: { message: "Successfully followed user" }, status: :created
+    else
+      render json: { errors: follow.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def unfollow
+    follow = Follow.find_by(follower_id: follow_params[:follower_id], followed_id: follow_params[:followed_id])
+
+    if follow
+      follow.destroy
+      render json: { message: "Successfully unfollowed the user" }, status: :ok
+    else
+      render json: { error: "Follow relationship not found" }, status: :not_found
+    end
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:name)
+  end
+
+  def follow_params
+    params.require(:follow).permit(:follower_id, :followed_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,12 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
-        resources :users, only: :create
+        resources :users, only: :create do
+          collection do
+            post :follow
+            delete :unfollow
+          end
+        end
       end
     end
   end
